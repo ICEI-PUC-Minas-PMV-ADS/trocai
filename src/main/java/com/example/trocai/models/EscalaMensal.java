2d1dae72d9e068a99c07ed6aa01184b591c3407e
suepuc <1337804@sga.pucminas.br>
2022-09-06 21:44:14 -0300

    Adds escala mensal

---
+++ src/main/java/com/example/trocai/models/EscalaMensal.java
@@ -0,0 +1,13 @@
+package com.example.trocai.models;
+
+import java.time.Month;
+import java.time.Year;
+import java.util.List;
+
+public class EscalaMensal {
+
+    private Year ano;
+    private Month mes;
+    private List<Dia> diasDeTrabalho;
+    private List<Dia> diasLivres;
+}
