9eececfe92e796d19db99ff328c48946340bd443
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-09-18 14:12:45 -0300

    add new colors

--- src/frontend/constants/Colors.ts
+++ src/frontend/constants/Colors.ts
@@ -1,12 +1,13 @@
 export default {
   light: {
-    red: "#F28482",
-    "light-blue": "#CEEAF7",
-    "dark-blue": "#457B9D",
-    black: "#141414",
-    gray: "#cccccc",
+    red: "#EE6C4D",
+    "light-blue": "#98C1D9",
+    "dark-blue": "#3D5A80",
+    black: "#293241",
+    gray: "#E0FBFC",
     "dark-gray": "#8e8e8e",
     yellow: "#F7D08A",
+    white: "#FFFFFF",
   },
   dark: {
     red: "#F28482",
