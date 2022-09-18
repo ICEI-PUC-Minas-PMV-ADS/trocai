19ff887990b3cdd9a5475ff23589d5e95603e006
Amanda <adnamasalles@gmail.com>
2022-09-18 01:32:29 -0300

    remove tela de login do spring

--- src/main/java/com/example/trocai/TrocaiApplication.java
+++ src/main/java/com/example/trocai/TrocaiApplication.java
@@ -5,13 +5,14 @@ import com.example.trocai.repositories.FuncionarioRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TrocaiApplication {
 
     public static void main(String[] args) {
