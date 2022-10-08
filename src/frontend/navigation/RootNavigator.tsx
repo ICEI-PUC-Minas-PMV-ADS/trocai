a8060c4790d2abbfbdbddc0e0b0df8db43715beb
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-10-08 14:53:35 -0300

    add routes to navigation

--- src/frontend/navigation/RootNavigator.tsx
+++ src/frontend/navigation/RootNavigator.tsx
@@ -1,12 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/login/loginScreen";
-import ModalScreen from "../screens/ModalScreen";
 import NotFoundScreen from "../screens/NotFoundScreen";
 import ProfileScreen from "../screens/profile/profileScreen";
 import { RootStackParamList } from "../types";
 import BottomTabNavigator from "./BottomTagNavigator";
 import HomePage from "../screens/home/homeScreen";
 import UserProfileForm from "../screens/profile/UserProfileForm";
+import NewRequest from "../screens/requests/newRequest";
 
 /**
  * A root stack navigator is often used for displaying modals on top of all other content.
@@ -43,13 +43,15 @@ function RootNavigator() {
         options={{ headerShown: false }}
       />
       <Stack.Screen
+        name="NewRequest"
+        component={NewRequest}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
         name="NotFound"
         component={NotFoundScreen}
         options={{ title: "Oops!" }}
       />
-      <Stack.Group screenOptions={{ presentation: "modal" }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group>
     </Stack.Navigator>
   );
 }
