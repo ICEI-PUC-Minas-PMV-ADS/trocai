66898d3cd39fb2f3bf873fdca49eeb5d722e86ff
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-10-08 14:00:57 -0300

    create main routes

---
+++ src/frontend/utils.ts
@@ -0,0 +1,9 @@
+export const MAIN_ROUTES = {
+  profile: "Profile",
+  freeShifts: "freeShifts",
+  myRequests: "myRequests",
+  newRequest: "newRequest",
+  fromOthers: "requestFromOthers",
+};
+
+export const filler = "erase later";
