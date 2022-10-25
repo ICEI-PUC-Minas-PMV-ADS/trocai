782ffbdc6618ea3d3c5b1e91a966794843d445ee
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-10-25 10:20:38 -0300

    lint utils

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -1,20 +1,23 @@
 import { RootStackParamList } from "./types";
 
-export const MAIN_ROUTES: Record<keyof RootStackParamList, keyof RootStackParamList> = {
-    Root: "Root",
-    NotFound: "NotFound",
-    Login: "Login",
-    Profile: "Profile",
-    SelectedUser: "SelectedUser",
-    EditProfile: "EditProfile",
-    Signup: "Signup",
-    AddUser: "AddUser",
-    UsersList: "UsersList",
-    HomeScreen: "HomeScreen",
-    FreeShifts: "FreeShifts",
-    MyRequests: "MyRequests",
-    NewRequest: "NewRequest",
-    RequestFromOthers: "RequestFromOthers",
+export const MAIN_ROUTES: Record<
+  keyof RootStackParamList,
+  keyof RootStackParamList
+> = {
+  Root: "Root",
+  NotFound: "NotFound",
+  Login: "Login",
+  Profile: "Profile",
+  SelectedUser: "SelectedUser",
+  EditProfile: "EditProfile",
+  Signup: "Signup",
+  AddUser: "AddUser",
+  UsersList: "UsersList",
+  HomeScreen: "HomeScreen",
+  FreeShifts: "FreeShifts",
+  MyRequests: "MyRequests",
+  NewRequest: "NewRequest",
+  RequestFromOthers: "RequestFromOthers",
 };
 
 export const filler = "erase later";
